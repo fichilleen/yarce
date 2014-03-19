5aad4b74a6049286641373e632204146be132480
Fich <fich@debian>
2014-03-19 09:23:47 +0000

    test_pieces.h: Previously didn't have a header file for test_pieces, and was including the cc file. Mostly function prototypes

---
+++ test_pieces.h
@@ -0,0 +1,6 @@
+#include "types.h"
+#include <string>
+
+void test_all_pieces ( const std::string &pieces );
+std::string piece_to_string ( const BitBoard bb );
+void run_piece_tests ( const Boards boards );
