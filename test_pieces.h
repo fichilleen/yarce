35f29ff52f8f903aacd59cbb6976eeb265d05d1b
Pauric@VM <pward@nyx.com>
2014-03-19 13:07:58 +0000

    test_pieces.h: Updated function prototypes

--- test_pieces.h
+++ test_pieces.h
@@ -2,5 +2,12 @@
 #include <string>
 
 void test_all_pieces ( const std::string &pieces );
-std::string piece_to_string ( const BitBoard bb );
+void test_all_white_pieces ( const std::string &pieces );
+void test_all_black_pieces ( const std::string &pieces );
+void test_white_pawns ( const std::string &pieces );
+void test_white_rooks ( const std::string &pieces );
+void test_no_conflicting_pieces ( const Boards boards );
+
+void piece_to_string ( std::string &result, const BitBoard bb );
 void run_piece_tests ( const Boards boards );
+void make_assertation ( const std::string &testname, const std::string &exp, const std::string &actual );
