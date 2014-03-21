829814b283be48527e056ca34f5a13421d7994c7
Pauric@VM <pward@nyx.com>
2014-03-21 13:30:34 +0000

    types.h: Added some directional marcos, typedef to represent them, const bitboards representing files

--- types.h
+++ types.h
@@ -24,4 +24,20 @@ typedef struct Boards {
     BitBoard all_pieces;
 } boards;
 
+const BitBoard file_a = 0x0101010101010101ULL;
+const BitBoard file_b = file_a << 1;
+const BitBoard file_c = file_a << 2;
+const BitBoard file_d = file_a << 3;
+const BitBoard file_e = file_a << 4;
+const BitBoard file_f = file_a << 5;
+const BitBoard file_g = file_a << 6;
+const BitBoard file_h = file_a << 7;
+
+// Directions
+typedef unsigned short direction;
+# define NORTHWEST 1
+# define NORTH 2
+# define NORTHEAST 3
+# define EAST 4
+
 #endif /* YARCE_TYPES_H */
